cf33e665191208f83592f98e18fe33c8ac0e7815
Alik <Nigay_AO@bk.ru>
2022-03-10 19:28:46 +0300

    Initial Commit

---
+++ MVVM SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM SwiftUI
+//
+//  Created by Alik Nigay on 10.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
